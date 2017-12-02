3c4181ef1074d8cc3b246abc41a09d7070e605f7
Harvey west <harvey_west@live.com>
2017-12-02 13:50:34 +0000

    Thread 0 has whole array to populate

--- parallel_distributed.c
+++ parallel_distributed.c
@@ -22,11 +22,11 @@ double **createArrayOfDoubles(int numberOfRows, int sizeOfRow)
 /* Fills each index with random double from 0.0 to 1 (not inclusive).
 */
 void populateBoundaryValues(double **array, int sizeOfRow, int startYIndex,
-                            int endYIndex, int numberOfRowsVisible)
+                            int endYIndex, int numberOfVisibleRows)
 {
     int y, x, endXIndex = sizeOfRow - 1;
 
-    for (y = 0; y < numberOfRowsVisible; y++)
+    for (y = 0; y < numberOfVisibleRows; y++)
     {
         array[y][0] = 1.0;
         array[y][endXIndex] = 1.0;
@@ -42,13 +42,14 @@ void populateBoundaryValues(double **array, int sizeOfRow, int startYIndex,
     {
         for (x = 1; x < endXIndex; x++)
         {
-            array[numberOfRowsVisible - 1][x] = 1.0;
+            array[numberOfVisibleRows - 1][x] = 1.0;
         }
     }
 }
+int getNumberOfVisibleRows(int myRank, int grainSize, int nproc);
 void printArray(double **array, int sizeOfRow);
 void printGrain(double **array, int offset, int grainSize, int sizeOfRow);
-void printVisibleArea(double **array, int numberOfRowsVisible, int sizeOfRow);
+void printVisibleArea(double **array, int numberOfVisibleRows, int sizeOfRow);
 
 int main(int argc, char **argv)
 {
@@ -58,44 +59,61 @@ int main(int argc, char **argv)
         countOfPasses = 0;
     double userPrecision;
     sscanf(argv[3], "%lf", &userPrecision);
-    int rc, myRank, nproc, namelen;
-    char name[MPI_MAX_PROCESSOR_NAME];
+    int rc, myRank, nproc;
+
     rc = MPI_Init(&argc, &argv);
     if (rc != MPI_SUCCESS)
     {
         printf("Error starting MPI program\n");
         MPI_Abort(MPI_COMM_WORLD, rc);
     }
+
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
     int startYIndex = myRank * grainSize,
         endYIndex = startYIndex + (grainSize - 1);
-    double **array;
+    double **workingArray, **destinationArray, **wholeArray;
+
     printf("hello world from ’%d’\n", myRank);
 
-    int numberOfRowsVisible = 0;
-    if (myRank == 0 || myRank == nproc - 1)
-    {
-        numberOfRowsVisible = grainSize + 1;
-    }
-    else
-    {
-        numberOfRowsVisible = grainSize + 2;
-    }
-    array = createArrayOfDoubles(numberOfRowsVisible, sizeOfRow);
-    populateBoundaryValues(array, sizeOfRow, startYIndex, endYIndex,
-                           numberOfRowsVisible);
+    int numberOfVisibleRows = getNumberOfVisibleRows(myRank, grainSize, nproc);
 
-    printGrain(array, myRank, grainSize, sizeOfRow);
-    // printVisibleArea(array,numberOfRowsVisible,sizeOfRow);
-    
+    workingArray = createArrayOfDoubles(numberOfVisibleRows, sizeOfRow);
+    destinationArray = createArrayOfDoubles(numberOfVisibleRows, sizeOfRow);
+
+    populateBoundaryValues(workingArray, sizeOfRow, startYIndex, endYIndex,
+                           numberOfVisibleRows);
+
+    // printGrain(array, myRank, grainSize, sizeOfRow);
+    // printVisibleArea(destinationArray,numberOfVisibleRows,sizeOfRow);
+
+    if (myRank == 0)
+    {
+        wholeArray = createArrayOfDoubles(sizeOfRow, sizeOfRow);
+        populateBoundaryValues(wholeArray, sizeOfRow, 0, sizeOfRow - 1,
+                               sizeOfRow);
+        printVisibleArea(wholeArray, sizeOfRow, sizeOfRow);
+    }
     /* implicit barrier in Finalize */
     /*MPI_Barrier(MPI_COMM_WORLD);*/
     MPI_Finalize();
     return 0;
 }
 
-
+int getNumberOfVisibleRows(int myRank, int grainSize, int nproc)
+{
+    int numberOfVisibleRows = 0;
+    if (myRank == 0 || myRank == nproc - 1)
+    {
+        numberOfVisibleRows = grainSize + 1;
+    }
+    else
+    {
+        numberOfVisibleRows = grainSize + 2;
+    }
+    return numberOfVisibleRows;
+}
 void printArray(double **array, int sizeOfRow)
 {
     int y, x;
@@ -132,11 +150,11 @@ void printGrain(double **array, int offset, int grainSize, int sizeOfRow)
     }
     printf("\n");
 }
-void printVisibleArea(double **array, int numberOfRowsVisible, int sizeOfRow)
+void printVisibleArea(double **array, int numberOfVisibleRows, int sizeOfRow)
 {
     //Would need a barrier to prevent other threads printing.
     int y, x;
-    for (y = 0; y < numberOfRowsVisible; y++)
+    for (y = 0; y < numberOfVisibleRows; y++)
     {
         for (x = 0; x < sizeOfRow; x++)
         {
